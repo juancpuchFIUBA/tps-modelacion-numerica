4a0eda3dcd93455db0614c47877d814e238c93e2
juanc_puch <juancpuch@gmail.com>
2024-05-02 00:26:32 -0300

    ya estan los 3 metodos

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -5,17 +5,33 @@ import matplotlib.pyplot as plt
 
 ROJO = '\033[91m'
 RESET = '\033[0m'
-TEMPERATURA_ARRIBA = 10
-TEMPERATURA_ABAJO = 11
-TEMPERATURA_IZQUIERDA = 12
-TEMPERATURA_DERECHA = 13
+TEMPERATURA_ARRIBA = 63
+TEMPERATURA_ABAJO = 44
+TEMPERATURA_IZQUIERDA = 26
+TEMPERATURA_DERECHA = 72
 LARGO_PLANCHA = 5
 ANCHO_PLANCHA = 4
-DISCRETIZACION = 1
+DISCRETIZACION = 0.5
+
+def imprimir_matriz (matriz):
+    f=0
+    for fila in matriz:
+        f+=1
+        c=0
+        for elemento in fila:
+            c+=1
+            if (c ==f): print (ROJO+ str(elemento)+ RESET, end=" " )
+            else: print (elemento, end=" ")
+        print("\n")
+
+def imprimir_vector (vector):
+    for elemento in vector:
+        print("{:.2f}".format(elemento), end=" ")
+    print("\n")
 
 def multiplicar_vector_constante(vector, constante, dimension):
     for i in range(dimension):
-        vector[i] = round(vector[i] * constante,2)
+        vector[i] = vector[i] * constante
     return vector
 def restar_vectores (vector_minuendo, vector_sustraendo, dimension):
     vector_resultado = []
@@ -51,27 +67,23 @@ def crear_placa_inicial (largo, ancho):
     return placa
 
 def crear_matriz_de_ceros (largo, ancho):
-    contador = 0
     cantidad_nodos = largo * ancho
     matriz = []
     for i in range(cantidad_nodos):
         fila_matriz = []
         for j in range(cantidad_nodos):
             fila_matriz.append(0)
-            contador = contador + 1
         matriz.append(fila_matriz)
     return matriz
 
 def no_es_borde_placha(posicion, dimension, largo_plancha, ancho_plancha):
     if (posicion < largo_plancha):
         return False
-
     busqueda = largo_plancha - 1
     while (busqueda < dimension - largo_plancha):
         if (posicion == busqueda or posicion == busqueda + 1):
             return False
         busqueda += largo_plancha
-
     if (posicion < dimension and posicion > dimension - largo_plancha):
         return False
     return True
@@ -90,60 +102,131 @@ def crear_matriz_A (largo, ancho):
             matriz[i][i] = 1
     return matriz
 
+def crear_vector_b (largo, ancho):
+    vector = []
+    for i in range(largo):
+        for j in range(ancho):
+            if (i == 0):
+                if (j == 0 or j == ancho - 1):
+                    vector.append(1)
+                else:
+                    vector.append(TEMPERATURA_ARRIBA)
+            elif (i == (largo - 1)):
+                if (j == 0 or j == ancho - 1):
+                    vector.append(1)
+                else:
+                    vector.append(TEMPERATURA_ABAJO)
+            elif (j == 0):
+                vector.append(TEMPERATURA_IZQUIERDA)
+            elif (j == (ancho - 1)):
+                vector.append(TEMPERATURA_DERECHA)
+            else:
+                vector.append(0)
+    return vector
 
-def imprimir_matriz (matriz):
-    f=0
-    for fila in matriz:
-        f+=1
-        c=0
-        for elemento in fila:
-            c+=1
-            if (c ==f): print (ROJO+ str(elemento)+ RESET, end=" " )
-            else: print (elemento, end=" ")
-        print("\n")
 
-def imprimir_vector (vector):
-    for elemento in vector:
-        print(round(float(elemento),2), end=" ")
-    print("\n")
+'''................................................................................'''
 
-def aplicar_eliminacion_gauseana (matriz, dimension):
+def aplicar_eliminacion_gauseana (matriz, dimension, vector):
+    A = np.array(matriz)
+    b = np.array(vector)
+    Ab = np.hstack((A, b.reshape(-1, 1)))
     for i in range(dimension):
-        vector_sustraendo = copy.deepcopy(matriz[i])
-        elemento_diagonal = matriz[i][i]
+        for j in range(i + 1, dimension):
+            c = Ab[j][i] / Ab[i][i]
+            for k in range(i, dimension+1):
+                Ab[j][k] -= c * Ab[i][k]
+    return Ab
 
-        print(f"FILA : {i}")
-        if (elemento_diagonal != 0):
-            for j in range(i+1, dimension):
-                elemento_eliminar = matriz[j][i]
+def sustitucion_inversa(matrix, vector, dimension):
+    x = np.zeros(dimension)
+    for i in range(dimension - 1, -1, -1):
+        b = vector[i]
+        variable = np.dot(matrix[i, i+1:], x[i+1:])
+        elemento = matrix[i, i]
+        x[i] = (b - variable) / elemento
+    return x
 
-                if (elemento_eliminar != 0):
-                    print(f"elemento eliminar : {elemento_eliminar}, fila: {j} columna: {i}")
+def resolucion_por_gauss (largo, ancho):
+    dimension = largo * ancho
+    A = crear_matriz_A(largo, ancho)
+    b = crear_vector_b(largo, ancho)
+    Ab = aplicar_eliminacion_gauseana(A, dimension, b)
+    A_con_eliminacion = Ab[:, :dimension]
+    b_con_eliminacion = Ab[:, dimension]
+    resultado = sustitucion_inversa(A_con_eliminacion, b_con_eliminacion, dimension)
+    return resultado
 
-                    multiplicador = round(elemento_eliminar/elemento_diagonal,2)
-                    if (multiplicador != 0 ):
-                        print(f"multiplicador : Eliminar {elemento_eliminar} * Diagonal {elemento_diagonal} = {multiplicador}")
-                        vector_sustraendo = multiplicar_vector_constante(vector_sustraendo, multiplicador, dimension)
-                        print(f"vector sustraendo ")
-                        imprimir_vector(vector_sustraendo)
-                        imprimir_vector(matriz[j])
-                        matriz[j] = restar_vectores(matriz[j], vector_sustraendo, dimension)
-                        imprimir_vector(matriz[j])
-    return matriz
+'''.....................................................................................'''
+def crear_solucion_inicial (dimension, elemento):
+    vector = []
+    for i in range (dimension):
+        vector.append(elemento)
+    return vector
+
+def aplicar_metodo_jacobi (matriz, vector, solucion_inicial, tolerancia, max_iteraciones, dimension):
+
+       x_acutal = np.copy(solucion_inicial)
+       x_siguiente = np.zeros_like(x_acutal)
+       A = np.array(matriz)
+       b = np.array(vector)
+       for j in range(max_iteraciones):
+           for i in range(dimension):
+               suma_ax = np.dot(A[i, :i], x_acutal[:i]) + np.dot(A[i, i + 1:], x_acutal[i + 1:])
+               x_siguiente[i] = (b[i] - suma_ax) / A[i, i]
 
 
+           if (np.sqrt(np.sum((x_siguiente - x_acutal) ** 2) / np.sqrt(np.sum(x_siguiente) ** 2)) < tolerancia):
+               return x_siguiente
+           x_acutal = np.copy(x_siguiente)
+       return 0
+def resolucion_por_jacobi (largo, ancho):
+    dimension = largo * ancho
+    A = crear_matriz_A(largo, ancho)
+    b = crear_vector_b(largo, ancho)
+    x0 = crear_solucion_inicial (dimension, 0)
+    tolerancia = 0.0000001
+    tolerancia_iteraciones = 100000000
+    x = aplicar_metodo_jacobi(A, b, x0, tolerancia, tolerancia_iteraciones, dimension)
+    return x
+
+'''.....................................................................................'''
+
+def aplicar_metodo_gauss_seidez(matriz, vector, solucion_inicial, tolerancia, max_iteraciones, dimension):
+    x_actual = np.copy(solucion_inicial)
+    x_siguiente = np.zeros_like(x_actual)
+    A = np.array(matriz)
+    b = np.array(vector)
+    for i in range (max_iteraciones):
+        for i in range (dimension):
+            x_siguiente[i] = (b[i] - np.dot(A[i, :i], x_siguiente[:i]) - np.dot(A[i, i + 1:], x_actual[i + 1:])) / A[i, i]
+
+        if (np.sqrt(np.sum((x_siguiente - x_actual) ** 2) / np.sqrt(np.sum(x_siguiente) ** 2)) < tolerancia):
+            return x_siguiente
+
+        x_actual = np.copy(x_siguiente)
+    return 0
+
+def resolucion_por_gauss_seidez(largo, ancho):
+    dimension = largo * ancho
+    A = crear_matriz_A(largo, ancho)
+    b = crear_vector_b(largo, ancho)
+    x0 = crear_solucion_inicial(dimension, 0)
+    tolerancia = 0.000001
+    tolerancia_iteraciones = 100000000
+    x = aplicar_metodo_gauss_seidez(A, b, x0, tolerancia, tolerancia_iteraciones, dimension)
+    return x
 
 def main ():
     largo_placa_uno = calcular_dimencion(LARGO_PLANCHA, DISCRETIZACION)
     ancho_placa_uno = calcular_dimencion(ANCHO_PLANCHA, DISCRETIZACION)
     placa_uno = crear_placa_inicial(largo_placa_uno, ancho_placa_uno)
-    imprimir_matriz(placa_uno)
-    A = crear_matriz_A(largo_placa_uno,ancho_placa_uno)
-    imprimir_matriz(A)
-    B = aplicar_eliminacion_gauseana(copy.deepcopy(A), largo_placa_uno * ancho_placa_uno)
-    print("pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp")
-    imprimir_matriz(B)
-
+    x = resolucion_por_gauss(largo_placa_uno, ancho_placa_uno)
+    y = resolucion_por_jacobi(largo_placa_uno, ancho_placa_uno)
+    z = resolucion_por_gauss_seidez(largo_placa_uno, ancho_placa_uno)
+    imprimir_vector(x)
+    imprimir_vector(y)
+    imprimir_vector(z)
     return 0
 
 main ()
--- ej 4 chatgpt.py
+++ ej 4 chatgpt.py
@@ -1,21 +1,32 @@
-import math
+def gauss_seidel(A, b, x0, tol, max_iter):
+    # Obtenemos la dimensión de la matriz A
+    n = len(b)
+    # Copiamos el vector inicial x0 para no modificarlo
+    x = x0.copy()
+    # Creamos un vector para almacenar la nueva iteración de x
+    x_new = np.zeros(n)
+    # Inicializamos el contador de iteraciones
+    iter_count = 0
 
-def C(x, n):
-    result = 0
-    for i in range(1, n+1):
-        result += (-1)**(i+1) * (x**(2*i-1)) / math.factorial(2*i-1)
-    return result
+    # Iteramos hasta que se cumpla alguna condición de parada
+    while True:
+        # Iteramos sobre cada fila de la matriz A
+        for i in range(n):
+            # Calculamos la nueva iteración de x[i]
+            x_new[i] = (b[i] - np.dot(A[i, :i], x_new[:i]) - np.dot(A[i, i + 1:], x[i + 1:])) / A[i, i]
 
-exacto = math.sin(math.pi/3)
-n = 1
-error = 1
-while error > 0.0001:
-    aproximado = C(math.pi/3, n)
-    error = abs((exacto - aproximado) / exacto)
-    n += 1
+        # Comprobamos si la norma de la diferencia entre x y x_new es menor que la tolerancia
+        # o si hemos alcanzado el número máximo de iteraciones
+        if np.linalg.norm(x_new - x) < tol or iter_count >= max_iter:
+            # Si se cumple alguna de las condiciones, salimos del bucle
+            break
 
-print("Número de términos necesarios:", n)
-print("Valor aproximado:", aproximado)
-print("Error relativo:", error)
+        # Actualizamos x con la nueva iteración calculada
+        x = x_new.copy()
+        # Incrementamos el contador de iteraciones
+        iter_count += 1
+
+    # Devolvemos la solución encontrada y el número de iteraciones realizadas
+    return x_new, iter_count
 
 
